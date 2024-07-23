421a7828ecd070b985a02a256911d9cd8b601549
ArielBitan <joekjoek33@gmail.com>
2024-07-23 15:29:20 +0300

    first commit

---
+++ 2024/23.07.24/homework/main.js
@@ -0,0 +1,11 @@
+// Declare two variables, firstName and lastName, and assign them your first and last name as string values.
+// Declare a variable age and assign it your age as a number.
+// Declare a variable isStudent and assign it a boolean value (true if you are a student, false otherwise).
+
+let firstName = "Ariel";
+let lastName = "Bitan";
+let age = 22;
+let isStudent = true;
+
+// Write a boolean expression to check if age is greater than 18. Assign the result to a variable isAdult.
+// Write a boolean expression to check if firstName is equal to "John". Assign the result to a variable isJohn.
--- 2024/23.07.24/main.js
+++
@@ -1,3 +0,0 @@
-console.log(50);
-console.log(100);
-console.log(50);
