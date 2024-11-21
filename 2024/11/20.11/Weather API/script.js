28534a8af35e0b133d02a08702be042ff0905cc3
ArielBitan <joekjoek33@gmail.com>
2024-11-21 13:56:31 +0200

    weather api finished(no styling)

--- 2024/11/20.11/Weather API/script.js
+++ 2024/11/20.11/Weather API/script.js
@@ -5,6 +5,7 @@ const weatherIconElement = document.getElementById("weatherIcon");
 const mainTempElement = document.querySelector(".temp");
 const tempDescElement = document.querySelector(".desc");
 const todaysTempElement = document.getElementById("today");
+const ulElement = document.getElementById("tempsList");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -27,10 +28,8 @@ async function getMainForecast(lat, lon) {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data);
     const imgCode = data.weather[0].icon;
-    weatherIconElement.src =
-      "http://openweathermap.org/img/w/" + imgCode + ".png";
+    weatherIconElement.src = `http://openweathermap.org/img/w/${imgCode}.png`;
     cityNameElement.innerHTML = data.name;
     mainTempElement.innerHTML = `${data.main.temp.toFixed(0)}°`;
     tempDescElement.innerHTML = data.weather[0].description;
@@ -40,6 +39,63 @@ async function getMainForecast(lat, lon) {
   } catch (err) {
     console.error("Error fetching weather data:", err);
   }
+}
+
+function createForecastData(dailyForecasts) {
+  const dailyMinTemps = [];
+  const dailyMaxTemps = [];
+  const dayNames = [];
+  const icons = [];
+  Object.values(dailyForecasts).forEach((item) => {
+    dailyMinTemps.push(Math.min(...item.temps).toFixed(0));
+    dailyMaxTemps.push(Math.max(...item.temps).toFixed(0));
+    icons.push(
+      item.icons[
+        item.temps.findIndex((value) => value === Math.max(...item.temps))
+      ]
+    );
+  });
+
+  Object.keys(dailyForecasts).forEach((item) =>
+    dayNames.push(getDayName(item))
+  );
+  createForecastItems(dailyMaxTemps, dailyMinTemps, dayNames, icons);
+}
+
+function createForecastItem(maxTemp, minTemp, dayName, icon) {
+  const liElement = document.createElement("li");
+  liElement.className = "list-item";
+
+  const iconContainerElement = document.createElement("div");
+  iconContainerElement.className = "icon-container";
+
+  const iconElement = document.createElement("img");
+  iconElement.src = `http://openweathermap.org/img/w/${icon}.png`;
+
+  const dayNameElement = document.createElement("span");
+  dayNameElement.textContent = dayName;
+
+  const maxMinTempElement = document.createElement("span");
+  maxMinTempElement.textContent = `${maxTemp}°/${minTemp}°`;
+
+  iconContainerElement.appendChild(iconElement);
+  iconContainerElement.appendChild(dayNameElement);
+  liElement.appendChild(iconContainerElement);
+  liElement.appendChild(maxMinTempElement);
+
+  return liElement;
+}
+
+function createForecastItems(maxTemps, minTemps, dayNames, icons) {
+  maxTemps.forEach((maxTemp, i) => {
+    const liElement = createForecastItem(
+      maxTemp,
+      minTemps[i],
+      dayNames[i],
+      icons[i]
+    );
+    ulElement.appendChild(liElement);
+  });
 }
 
 async function getDailyForecasts(lat, lon) {
@@ -57,13 +113,29 @@ async function getDailyForecasts(lat, lon) {
           icons: [],
         };
       }
+
       dailyForecasts[date].temps.push(element.main.temp);
       dailyForecasts[date].icons.push(element.weather[0].icon);
-      console.log(dailyForecasts);
     });
+    createForecastData(dailyForecasts);
   } catch (err) {
     console.error("Error fetching weather data:", err);
   }
+}
+
+function getDayName(dateStr) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const date = new Date(dateStr);
+  var dayName = days[date.getDay()];
+  return dayName;
 }
 
 getLocation();
