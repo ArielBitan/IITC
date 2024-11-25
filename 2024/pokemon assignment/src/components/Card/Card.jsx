9ef0f78630f2cf1bde5385a27733578b693b464e
ArielBitan <joekjoek33@gmail.com>
2024-11-25 11:03:12 +0200

    ד

--- 2024/pokemon assignment/src/components/Card/Card.jsx
+++ 2024/pokemon assignment/src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import styles from "./Card.module.css";
 const Card = (props) => {
   return (
     <>
-      <li class="bg-gray-300 p-2 rounded-md">
+      <li class="bg-gray-600 p-2 rounded-md">
         <h3>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</h3>
         <div class="flex items-center justify-evenly gap-2">
           {props.types.map((element, index) => (
